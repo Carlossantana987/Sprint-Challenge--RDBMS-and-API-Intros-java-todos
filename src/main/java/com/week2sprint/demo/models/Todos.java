e133e973e3a4257c4acfa2d016da46e45a5caab4
Carlossantana987 <carlossantana987@outlook.com>
2019-07-19 11:34:14 -0400

    Classes for models package added

---
+++ src/main/java/com/week2sprint/demo/models/Todos.java
@@ -0,0 +1,78 @@
+package com.week2sprint.demo.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.catalina.User;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "quotes")
+public class Todos
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoId;
+
+    private String description;
+    private boolean completed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid",
+                nullable = false)
+    @JsonIgnoreProperties({"quotes", "hibernateLazyInitializer"})
+    private User user;
+
+    public Todos()
+    {
+
+    }
+
+    public Todos(String description, boolean completed, User user)
+    {
+        this.description = description;
+        this.completed = completed;
+        this.user = user;
+    }
+
+    public long getTodoId()
+    {
+        return todoId;
+    }
+
+    public void setTodoId(long todoId)
+    {
+        this.todoId = todoId;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public boolean isCompleted()
+    {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed)
+    {
+        this.completed = completed;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+}
